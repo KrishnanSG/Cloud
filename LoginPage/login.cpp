2bbd5823878844c9562493fd4afeab8b10770d93
Krishnan S G <43802499+KrishnanSG@users.noreply.github.com>
2019-01-26 01:45:01 -0800

    Psedo Login Page & FeedPage

---
+++ LoginPage/login.cpp
@@ -0,0 +1,30 @@
+#include "login.h"
+#include "welcome.h"
+#include "ui_login.h"
+Login::Login(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Login)
+{
+    ui->setupUi(this);
+}
+
+Login::~Login()
+{
+    delete ui;
+}
+
+void Login::on_pushButton_clicked()
+{
+    QString username,password;
+    username=ui->lineEdit->text();
+    password=ui->lineEdit_2->text();
+    ui->lineEdit->setText(password);
+    if (username==password)
+    {
+        hide();
+        Welcome w;
+        w.showMaximized();
+        w.exec();
+        show();
+    }
+}
